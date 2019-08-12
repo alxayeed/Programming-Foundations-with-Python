fe63c8f49701a9b5e084d028e0d64501b4f65ab2
alxayeed <alxayeed@gmail.com>
2019-08-12 22:33:26 +0600

    movie app of a movie website is created

--- movie/media.py
+++ movie/media.py
@@ -1,4 +1,5 @@
 #Movie details
+import webbrowser
 
 class Movie():
 
@@ -6,7 +7,13 @@ class Movie():
 		self.title = title
 		self.info = info
 		self.poster = 'al.jpg'
-		slef.trailer = 'trailer.mp4'
+		self.trailer = 'trailer.mp4'
+
+	def show_trailer(self):
+		webbrowser.open(self.trailer)
+	def show_poster(self):
+		webbrowser.open(self.poster)
+
 
 		
 
--- movie/movie_details.py
+++ movie/movie_details.py
@@ -1,6 +1,11 @@
 #Movie details
 
 import media
+import time
 
-toy_story = media.Movie()
-toy_story.hello()
+toy_story = media.Movie('24th July','birthday of Al','Al.jpg','F:\Practice\Python\Courses\Programming Foundations with Python Videos\movie\trailer.mp4')
+print(toy_story.title)
+toy_story.show_poster()
+time.sleep(10)
+toy_story.show_trailer()
+
